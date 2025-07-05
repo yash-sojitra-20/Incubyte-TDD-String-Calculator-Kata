01f0d7bbb1fd894007823efcfda403157317c889
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2025-07-05 16:08:36 +0530

    test: add failing test case for single number input

--- src/test/java/com/example/calculator/StringCalculatorTest.java
+++ src/test/java/com/example/calculator/StringCalculatorTest.java
@@ -11,4 +11,12 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculatorImpl();
         assertEquals(0, calculator.add(""));
     }
+
+    @Test
+    public void testSingleNumberReturnsItsValue() {
+        StringCalculator calculator = new StringCalculatorImpl();
+        assertEquals(1, calculator.add("1"));
+        assertEquals(42, calculator.add("42"));
+    }
+
 }
