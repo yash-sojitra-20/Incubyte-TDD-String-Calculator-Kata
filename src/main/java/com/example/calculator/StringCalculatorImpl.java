162109a1fb472b813170ee07de6bf7b304648b42
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2025-07-05 15:21:38 +0530

    Implemented add() method in StringCalculatorImpl class

---
+++ src/main/java/com/example/calculator/StringCalculatorImpl.java
@@ -0,0 +1,9 @@
+package com.example.calculator;
+
+public class StringCalculatorImpl implements StringCalculator {
+
+    @Override
+    public int add(String numbers) {
+        return 0; // placeholder for first test
+    }
+}
